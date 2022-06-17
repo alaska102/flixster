1517841350f1606768b04cd74e5cd04b43f765fd
alaska102 <68394076+alaska102@users.noreply.github.com>
2022-06-17 16:07:25 -0700

    lots and lots

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-📝 `NOTE` Use this template to initialize the contents of a README.md file for your application. As you work on your assignment over the course of the week, update the required or stretch features lists to indicate which features you have completed by changing `[ ]` to `[x]`. (🚫 Remove this paragraph before submitting your assignment.)
-
 ## Week 1 Assignment: Flixster
 
-Submitted by: **NAME**
+Submitted by: **Hailey Garcia**
 
-Estimated time spent: **#** hours spent in total
+Estimated time spent: **40** hours spent in total
 
 Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
 
@@ -12,33 +10,33 @@ Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
 
 #### CORE FEATURES
 
-- [ ] User can view a list of current movies from The Movie Database API as a grid view
+- [X] User can view a list of current movies from The Movie Database API as a grid view
   - The grid element should have an id of `movies-grid`
   - Each movie wrapper element should have a class of `movie-card`
-- [ ] For each movie displayed, user can see the following details:
+- [X] For each movie displayed, user can see the following details:
   - Title - the element should have a class of `movie-title`
   - Image - the `img` element should have a class of `movie-poster`
   - Votes - the element should have a class of `movie-votes`
-- [ ] User can load more current movies by clicking a button at the bottom of the list
+- [X] User can load more current movies by clicking a button at the bottom of the list
   - The button should have an id of `load-more-movies-btn`.
   - When clicked, the page should not refresh.
   - New movies should simply be added to the bottom
-- [ ] Allow users to search for movies and display them in a grid view
+- [X] Allow users to search for movies and display them in a grid view
   - There should be a search input element with an id of `search-input`
   - Users should be able to type into the input
   - When a user hits 'Enter', it should send a search request to the movies API
   - The results from the search should be displayed on the page
   - There should be a close icon with an id of `close-search-btn` that exits the search, clears results, and shows the current movies displayed previously
-- [ ] Website accounts for basic HTML/CSS accessibility features
-- [ ] Website should be responsive
+- [X] Website accounts for basic HTML/CSS accessibility features
+- [X] Website should be responsive
 
 #### STRETCH FEATURES
 
-- [ ] Deploy website using GitHub Pages. 
-- [ ] Allow user to view more details about a movie within a popup.
-- [ ] Improve the user experience through CSS & animation.
+- [X] Deploy website using GitHub Pages. 
+- [X] Allow user to view more details about a movie within a popup.
+- [X] Improve the user experience through CSS & animation.
 - [ ] Allow movie video trailers to be played using [embedded YouTube](https://support.google.com/youtube/answer/171780?hl=en)
-- [ ] Implement anything else that you can get done to improve the app functionality!
+- [X] Implement anything else that you can get done to improve the app functionality!
 
 ### Walkthrough Video
 
@@ -50,15 +48,15 @@ Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
 
 * Did the topics discussed in your labs prepare you to complete the assignment? Be specific, which features in your weekly assignment did you feel unprepared to complete?
 
-Add your response here
+This week was my first of ever using Javascript. Although both the Report Card and Giphy labs gave me the skills I needed to complete this project, I struggled a lot. Specifically, I think the stretch feature of creating the additional info popup boxes and the videos were really challenging to me. I also felt a bit unprepared when creating the movie grids, as I didn't have much CSS practice. 
 
 * If you had more time, what would you have done differently? Would you have added additional features? Changed the way your project responded to a particular event, etc.
   
-Add your response here
+I would have improved the layout of the popup boxes. I definetly think I could have improved there with making the UI look more visually appealing. Additionally, I would have liked to add cursor effects. 
 
 * Reflect on your project demo, what went well? Were there things that maybe didn't go as planned? Did you notice something that your peer did that you would like to try next time?
 
-Add your response here
+I'm really proud of how the movie grid turned out. It's exactly what I envisioned when I made it, and it completely represents my personal style :) However, I struggled a lot with the backend work. Using Javascript & APIs for the first time was a significant challenge but it was really cool to explore. One thing my friend did was letting the videos play in fullscreen and I think that would be a really cool feature to try exploring, especially iframes. 
 
 ### Open-source libraries used
 
@@ -67,3 +65,4 @@ Add your response here
 ### Shout out
 
 Give a shout out to somebody from your cohort that especially helped you during your project. This can be a fellow peer, instructor, TA, mentor, etc.
+
--- script.js
+++ script.js
@@ -1,116 +1,131 @@
+// GLOBAl CONSTANTS
+const API_KEY = "8fb52c5ec6da0eeeaf25da9b9268f83e"
+const BASE_API_URL  = "https://api.themoviedb.org/3"
+const API_KEY_QUERY = "?api_key=" + API_KEY
+ 
+let searchTerm = "";
+let currentPage = 1;
+let latestApiCall = ""
+ 
+const moviesGrid = document.querySelector("#movies-grid");
+const searchBtn = document.querySelector("#clicker");
+const closeSearchBtn = document.querySelector("#close-search-btn");
+const loadMoreBtn = document.querySelector("#load-more-movies-btn");
+const popupContainer = document.querySelector("#popup-container");
+const popupEl = document.querySelector("#popup");
 
-const movies = [
-   {
-   id: 338953,
-   posterPath: "/8ZbybiGYe8XM4WGmGlhF0ec5R7u.jpg",
-   title: "Fantastic Beasts: The Secrets of Dumbledore",
-   voteAverage: 6.9
-   },
-   {
-   id: 526896,
-   posterPath: "/6JjfSchsU6daXk2AKX8EEBjO3Fm.jpg",
-   title: "Morbius",
-   voteAverage: 6.4
-   },
-   {
-   id: 752623,
-   posterPath: "/neMZH82Stu91d3iqvLdNQfqPPyl.jpg",
-   title: "The Lost City",
-   voteAverage: 6.8
-   },
-   {
-   id: 675353,
-   posterPath: "/6DrHO1jr3qVrViUO6s6kFiAGM7.jpg",
-   title: "Sonic the Hedgehog 2",
-   voteAverage: 7.7
-   },
-   {
-   id: 639933,
-   posterPath: "/zhLKlUaF1SEpO58ppHIAyENkwgw.jpg",
-   title: "The Northman",
-   voteAverage: 7.3
-   },
-   {
-   id: 818397,
-   posterPath: "/QaNLpq3Wuu2yp5ESsXYcQCOpUk.jpg",
-   title: "Memory",
-   voteAverage: 7.3
-   },
-   {
-   id: 507086,
-   posterPath: "/kAVRgw7GgK1CfYEJq8ME6EvRIgU.jpg",
-   title: "Jurassic World Dominion",
-   voteAverage: 6.7
-   },
-   {
-   id: 453395,
-   posterPath: "/9Gtg2DzBhmYamXBS1hKAhiwbBKS.jpg",
-   title: "Doctor Strange in the Multiverse of Madness",
-   voteAverage: 7.4
-   },
-   {
-   id: 831946,
-   posterPath: "/cpWUtkcgRKeauhTyVMjYHxAutp4.jpg",
-   title: "Interceptor",
-   voteAverage: 6.3
-   },
-   {
-   id: 610150,
-   posterPath: "/rugyJdeoJm7cSJL1q4jBpTNbxyU.jpg",
-   title: "Dragon Ball Super: Super Hero",
-   voteAverage: 6.8
-   },
-   {
-   id: 414906,
-   posterPath: "/74xTEgt7R36Fpooo50r9T25onhq.jpg",
-   title: "The Batman",
-   voteAverage: 7.8
-   },
-   {
-   id: 628900,
-   posterPath: "/rJPGPZ5soaG27MK90oKpioSiJE2.jpg",
-   title: "The Contractor",
-   voteAverage: 6.6
-   },
-   {
-   id: 629542,
-   posterPath: "/7qop80YfuO0BwJa1uXk1DXUUEwv.jpg",
-   title: "The Bad Guys",
-   voteAverage: 7.8
-   },
-   {
-   id: 825808,
-   posterPath: "/g2n1lFIFXC0lpG32ysUhFi0Uz61.jpg",
-   title: "See for Me",
-   voteAverage: 6
-   },
-   {
-   id: 763285,
-   posterPath: "/zT5ynZ0UR6HFfWQSRf2uKtqCyWD.jpg",
-   title: "Ambulance",
-   voteAverage: 7
-   },
-   {
-   id: 648579,
-   posterPath: "/bmxCAO0tz79xn40swJAEIJPRnC1.jpg",
-   title: "The Unbearable Weight of Massive Talent",
-   voteAverage: 7.3
-   },
-   {
-   id: 361743,
-   posterPath: "/wxP2Mzv9CdjOK6t4dNnFGqIQl0V.jpg",
-   title: "Top Gun: Maverick",
-   voteAverage: 8.3
-   }
-];
+searchBtn.addEventListener("click", handleSearchFormSubmit);
+closeSearchBtn.addEventListener("click", closeSearch);
+loadMoreBtn.addEventListener("click", loadMoreMovies);
+popupContainer.addEventListener("click", hidePopup);
 
-const movieGridElem = document.querySelector("#movie-grid")
-function printMovies(movie) {
-    // movieGridElem.innerHTML = movieGridElem.innerHTML + '
-    // <div id = "movie-grid">
-    //     <p>${movie.title}</p>
-    //     <p> Vote Average:  ${movie.voteAverage}
-    //     </p>
-    // </div>
-    // '
+
+async function callAPI (apiRequestURL){
+    let response = await fetch(apiRequestURL);
+    let responseData = await response.json();
+    return responseData;
 }
+
+async function getResults (apiRequestURL){
+    latestApiCall = apiRequestURL
+    results = await callAPI(apiRequestURL)
+    return results
+}
+
+function displayMovieResults (resultsData){
+    movieResults = resultsData.results; 
+    movieResults.forEach( movie => createMovieCard(movie))
+}
+
+async function displayNowPlaying (){
+    movieResults = await getResults(BASE_API_URL + "/movie/now_playing" + API_KEY_QUERY);
+    displayMovieResults(results);
+ }
+
+function createMovieCard (movieObject){
+    movieCardID = "movie-"+movieObject.id;
+
+    let movieCardDiv = document.createElement('div');
+    movieCardDiv.className = "movie-card";
+    document.getElementById("movies-grid").appendChild(movieCardDiv);
+
+    let posterPath = movieObject.poster_path
+    posterDisplayURL = (posterPath ? "https://image.tmdb.org/t/p/original"+posterPath : "https://fl-1.cdn.flockler.com/embed/no-image.svg") 
+    
+    movieCardDiv.innerHTML = `
+        <img class="movie-poster"
+            src="${posterDisplayURL}"
+            alt="${movieObject.title} (${movieObject.year}) poster image"
+            movieid="${movieCardID}"
+            >
+        <div class="half-overlay" movieid="${movieCardID}"}">
+            <div class="movie-card-info" movieid="${movieCardID}">
+                <h3 class="movie-votes" movieid="${movieCardID}">★ ${movieObject.vote_average}</h3>
+                <h2 class="movie-title" movieid="${movieCardID}">${movieObject.title}</h2>
+            </div>
+        </div>`;
+
+    movieCardDiv.addEventListener('click', handleMovieCardClick)
+}
+
+ async function handleSearchFormSubmit (event){
+    event.preventDefault();
+    moviesGrid.innerHTML = "";
+    currentPage = 1;
+    
+    searchTerm = document.getElementById('search-input').value;
+    movieResults = await getResults(BASE_API_URL + "/search/movie" + API_KEY_QUERY + "&query=" + searchTerm)
+    displayMovieResults(movieResults)
+    closeSearchBtn.classList.remove("hidden");
+ }
+
+ function closeSearch (event){
+    moviesGrid.innerHTML = "";
+    currentPage = 1;
+    displayNowPlaying();
+    closeSearchBtn.classList.add("hidden");
+ }
+ 
+
+
+ async function handleMovieCardClick (event) {
+    movieID = (event.target.attributes.movieid.value).slice(6);
+    movieObject = await callAPI(BASE_API_URL + "/movie/" + movieID + API_KEY_QUERY);
+    movieVideoObject = await callAPI(BASE_API_URL + "/movie/" + movieID + "videos" + API_KEY_QUERY);
+
+    popupEl.innerHTML = `
+        <img class="movie-header" src="https://image.tmdb.org/t/p/w500${movieObject.poster_path}">
+        <div id = "more-info">
+        <h1>${movieObject.title}</h1>
+        <span>★ ${movieObject.vote_average}/10</span>
+        <br><br>
+        <p>${movieObject.overview}</p>
+        <br><br>
+        <iframe> "https://www.youtube.com/embed/${movieVideoObject.id}"
+        </div>
+    `
+    console.log(movieObject.poster_path)
+    showPopup();
+}
+
+function showPopup (event) {
+    popupContainer.classList.remove("hidden");
+    popupEl.classList.remove("hidden");
+}
+
+function hidePopup (event) {
+    popupContainer.classList.add("hidden");
+    popupEl.classList.add("hidden");
+}
+ 
+async function loadMoreMovies (){
+    console.log('clicked')
+    currentPage += 1
+    results = await getResults(latestApiCall+"&page=" + currentPage)
+    displayMovieResults(results)
+ }
+
+ window.onload = function () {
+    currentPage = 1;
+    displayNowPlaying();
+   }
